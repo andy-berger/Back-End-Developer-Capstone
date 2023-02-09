834c1bab850f49d30147f9ae9baca25c1eb7e86e
Andy Berger <inocid@gmail.com>
2023-02-09 14:53:25 +0100

    Adding tests

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -5,7 +5,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
 class MenuItemsView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
